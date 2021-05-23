541b4edff82aa5c57a7d88faff6002930023cb11
aleks-nikolov <a.nikolov@studenti.unina.it>
2021-05-23 18:17:12 +0200

    Some cleaning up

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import "../Style/App.css";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import "../style/App.css";
 
 
 class App extends Component {
@@ -25,20 +26,26 @@ class App extends Component {
 	}
 
 	render() {
-		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+		const {robots, searchField} = this.state;
+
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField.toLowerCase())
 		})
 
-		if (this.state.robots.length === 0) {
+		if (!robots.length) {
 			return <h1 className="tc">Loading</h1>
 		} else {
 			return (
-			<div className="tc">
-				<h1 className="f1">RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots = {filteredRobots} />
-			</div>
-		);
+				<div className="tc">
+					<h1 className="f1">RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+
+					<Scroll>
+						<CardList robots = {filteredRobots} />
+					</Scroll>
+
+				</div>
+			);
 		}
 	}
 }
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Style/index.css';
-import App from "./Components/App";
+import './style/index.css';
+import App from "./containers/App";
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
