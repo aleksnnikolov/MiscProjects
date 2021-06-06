e5aa774f2d71ef558bebb915b027854dd44d6a26
aleksnnikolov <aleniknikolov@gmail.com>
2021-06-06 16:33:09 +0200

    added login and register forms, plus some polish to some components

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import SignIn from './components/SignIn/SignIn.js';
+import Register from './components/Register/Register.js';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import Rank from './components/Rank/Rank.js';
@@ -9,7 +11,7 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 
 const app = new Clarifai.App({
- apiKey: 'API_KEY'
+ apiKey: '1fbf857022ce4169bffc8cb00122bf73'
 });
 
 const particlesOptions = {
@@ -29,8 +31,30 @@ class App extends React.Component {
     super();
     this.state = {
       input: "",
-      imageUrl: ""
+      imageUrl: "",
+      box: {},
+      route: "signin",
+      isSignedIn: false
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const facePosition = data.outputs[0].data.regions[0].region_info.bounding_box;
+
+    const image = document.getElementById("inputimage");
+    const imageWidth = Number(image.width);
+    const imageHeight = Number(image.height);
+
+    return {
+      leftCol: facePosition.left_col * imageWidth,
+      topRow: facePosition.top_row * imageHeight,
+      rightCol: imageWidth - (facePosition.right_col * imageWidth),
+      bottomRow: imageHeight - (facePosition.bottom_row * imageHeight)
+    }
+  }
+
+  displayFaceBox = (boxData) => {
+    this.setState({box: boxData});
   }
 
   onInputChange = (event) => {
@@ -41,27 +65,40 @@ class App extends React.Component {
     this.setState({imageUrl: this.state.input});
 
     app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-              .then(
-                function(response) {
-                  console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-                },
-                function(err) {
+              .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+              .catch(err => console.log(err));
+  }
 
-                }
-              );
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({isSignedIn: false});
+    } else if (route === "home") {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
   }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions}/>
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange} 
-          onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn}/>
+        { route === "home"
+          ? <div> 
+              <Logo />
+              <Rank />
+              <ImageLinkForm 
+                onInputChange={this.onInputChange} 
+                onButtonSubmit={this.onButtonSubmit}/>
+              <FaceRecognition box={box} imageUrl={imageUrl}/>
+            </div>
+          : (
+              route === "signin"
+              ? <SignIn onRouteChange={this.onRouteChange} />
+              : <Register onRouteChange={this.onRouteChange} />
+            )
+        }
       </div>
     );
   }
--- smartbrain/src/components/FaceRecognition/FaceRecognition.js
+++ smartbrain/src/components/FaceRecognition/FaceRecognition.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ box, imageUrl }) => {
 	return (
 		<div className="center ma">
-			<div className="absolite mt2">
-				<img alt="" src={imageUrl} width="500px" height="auto"/>
+			<div className="absolute mt2">
+				<img id="inputimage" alt="" src={imageUrl} width="500px" height="auto"/>
+				<div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
 			</div>
 		</div>
 	);
