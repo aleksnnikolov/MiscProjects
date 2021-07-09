3010040411f562a5f6b318ffe16d867ab4eb4d83
aleksnnikolov <aleniknikolov@gmail.com>
2021-07-09 14:25:33 +0200

    refactoring and code review

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -11,7 +11,7 @@ import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 
 const app = new Clarifai.App({
- apiKey: 'API KEY'
+ apiKey: 'API_KEY'
 });
 
 const particlesOptions = {
@@ -26,10 +26,7 @@ const particlesOptions = {
   }
 }
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
+const initialState = {
       input: "",
       imageUrl: "",
       box: {},
@@ -42,7 +39,12 @@ class App extends React.Component {
         entries: 0,
         joined: ""
       }
-    }
+    };
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -94,7 +96,8 @@ class App extends React.Component {
                   .then(response => response.json())
                   .then(count => {
                     this.setState(Object.assign(this.state.user, {entries: count}))
-                  });
+                  })
+                  .catch(console.log);
                 }
                 this.displayFaceBox(this.calculateFaceLocation(response))
               })
@@ -103,7 +106,7 @@ class App extends React.Component {
 
   onRouteChange = (route) => {
     if (route === "signout") {
-      this.setState({isSignedIn: false});
+      this.setState(initialState);
     } else if (route === "home") {
       this.setState({isSignedIn: true})
     }
